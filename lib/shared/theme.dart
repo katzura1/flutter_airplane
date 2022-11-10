b08347fa6f0610fb9ee6e363582887573f382d12
Denny <denny.az45@gmail.com>
2022-11-10 22:54:22 +0700

    + setup global theme variable + slicing splash, sign up

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Color kPrimaryColor = const Color(0xff5C40CC);
 Color kBlackColor = const Color(0xff1F1449);
@@ -9,25 +10,25 @@ Color kRedColor = const Color(0xffEB70A5);
 Color kBackgroundColor = const Color(0xffFAFAFA);
 Color kInActiveColor = const Color(0xffDBD7EC);
 
-TextStyle primaryTextStyle = const GoogleFonts.poppins(
+TextStyle primaryTextStyle = GoogleFonts.poppins(
   color: kPrimaryColor,
 );
 
-TextStyle blackTextStyle = const GoogleFonts.poppins(
+TextStyle blackTextStyle = GoogleFonts.poppins(
   color: kBlackColor,
 );
 
-TextStyle whiteTextStyle = const GoogleFonts.poppins(
+TextStyle whiteTextStyle = GoogleFonts.poppins(
   color: kWhiteColor,
 );
 
-TextStyle grayTextStyle = const GoogleFonts.poppins(
+TextStyle grayTextStyle = GoogleFonts.poppins(
   color: kGrayColor,
 );
-TextStyle greenTextStyle = const GoogleFonts.poppins(
+TextStyle greenTextStyle = GoogleFonts.poppins(
   color: kGreenColor,
 );
-TextStyle redTextStyle = const GoogleFonts.poppins(
+TextStyle redTextStyle = GoogleFonts.poppins(
   color: kRedColor,
 );
 
@@ -38,3 +39,6 @@ FontWeight semiBold = FontWeight.w600;
 FontWeight bold = FontWeight.w700;
 FontWeight extraBold = FontWeight.w800;
 FontWeight black = FontWeight.w900;
+
+double defaultMargin = 24.0;
+double defaultRadius = 17.0;
---
+++ lib/ui/pages/sign_up_page.dart
@@ -0,0 +1,224 @@
+import 'package:flutter/material.dart';
+import '../../shared/theme.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget title() {
+      return Container(
+        margin: const EdgeInsets.only(top: 30),
+        child: Text(
+          "Join us and get\nyour next journey",
+          style: blackTextStyle.copyWith(
+            fontSize: 24,
+            fontWeight: semiBold,
+          ),
+        ),
+      );
+    }
+
+    Widget inputSecion() {
+      Widget nameInput() {
+        return Container(
+          margin: const EdgeInsets.only(
+            bottom: 20,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text("Full Name"),
+              const SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                cursorColor: kBlackColor,
+                decoration: InputDecoration(
+                  hintText: "Your Full Name",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(defaultRadius),
+                      borderSide: BorderSide(
+                        color: kPrimaryColor,
+                      )),
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+
+      Widget emailInput() {
+        return Container(
+          margin: const EdgeInsets.only(
+            bottom: 20,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text("Email Address"),
+              const SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                cursorColor: kBlackColor,
+                decoration: InputDecoration(
+                  hintText: "Your Email Address",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(defaultRadius),
+                      borderSide: BorderSide(
+                        color: kPrimaryColor,
+                      )),
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+
+      Widget passwordInput() {
+        return Container(
+          margin: const EdgeInsets.only(
+            bottom: 20,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text("Password"),
+              const SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                cursorColor: kBlackColor,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Your Password",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(defaultRadius),
+                      borderSide: BorderSide(
+                        color: kPrimaryColor,
+                      )),
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+
+      Widget hobbyInput() {
+        return Container(
+          margin: const EdgeInsets.only(
+            bottom: 30,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text("Hobby"),
+              const SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                cursorColor: kBlackColor,
+                decoration: InputDecoration(
+                  hintText: "Your Hobby",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(defaultRadius),
+                      borderSide: BorderSide(
+                        color: kPrimaryColor,
+                      )),
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+
+      Widget submitButton() {
+        return Container(
+          width: double.infinity,
+          height: 55,
+          child: TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                backgroundColor: kPrimaryColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(defaultRadius),
+                ),
+              ),
+              child: Text(
+                "Get Started",
+                style: whiteTextStyle.copyWith(
+                  fontSize: 18,
+                  fontWeight: medium,
+                ),
+              )),
+        );
+      }
+
+      return Container(
+        margin: const EdgeInsets.only(top: 30),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 30,
+        ),
+        decoration: BoxDecoration(
+          color: kWhiteColor,
+          borderRadius: BorderRadius.circular(defaultRadius),
+        ),
+        child: Column(
+          children: [
+            nameInput(),
+            emailInput(),
+            passwordInput(),
+            hobbyInput(),
+            submitButton(),
+          ],
+        ),
+      );
+    }
+
+    Widget tacButton() {
+      return Container(
+        alignment: Alignment.center,
+        margin: EdgeInsets.only(
+          top: 50,
+          bottom: 73,
+        ),
+        child: Text("Terms and Conditions",
+            style: grayTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: light,
+              decoration: TextDecoration.underline,
+            )),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(
+            horizontal: defaultMargin,
+          ),
+          children: [
+            title(),
+            inputSecion(),
+            tacButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
